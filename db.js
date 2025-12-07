17d36edee18acfbaf27efaddebd1cf55ccb2a789
Jasumy <jazmin.segundo.e@gmail.com>
2025-12-07 15:48:09 -0600

    Conectando backend con MySQL de Railway

--- db.js
+++ db.js
@@ -1,18 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// creamos un 'pool' de conexiones para manejar varias peticiones a la vez
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306, // ← Railway usa 3306 interno
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// exportamos la conexión usando promesas (async/await) para que sea moderno
 const db = pool.promise();
-
 module.exports = db;
